f9160f1ca4e6105f2f76c627e7bc754ac4e71891
Scott <scotti.fletcher.1@gmail.com>
2025-06-25 09:37:15 +1000

    add bootstrap script

--- run_in_cloudshell.sh
+++ run_in_cloudshell.sh
@@ -5,3 +5,12 @@ mkdir ~/bin
 ln -s ~/.tfenv/bin/* ~/bin/
 tfenv install
 tfenv use 1.12.2
+
+git clone https://github.com/scotti-fletcher/injection-demo
+cd injection-demo/terraform
+terraform init
+terraform apply --auto-approve
+export WEBSERVER_IP=$(terraform output -raw webserver_public_ip)
+export ATTACKER_IP=$(terraform output -raw attacker_public_ip)
+cd ~/
+ssh -i ~/lab_key.pem ubuntu@$WEBSERVER_IP
