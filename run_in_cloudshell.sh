21a2a19837131e8b383f6dde69cad4cf5893a149
Scotti Fletcher <56489803+scotti-fletcher@users.noreply.github.com>
2025-06-25 16:23:54 +1000

    Update run_in_cloudshell.sh

--- run_in_cloudshell.sh
+++ run_in_cloudshell.sh
@@ -11,4 +11,3 @@ terraform init
 terraform apply --auto-approve
 cd ~/
 echo "Please wait 60 seconds for the EC2 instances to finish provisioning before connecting via SSH to the WebServer using CloudShell"
-sleep 60 #wait for the EC2s to be ready to accept SSH connections
